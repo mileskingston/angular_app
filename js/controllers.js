4c45604373ae794644e1d4806ee8e79dc3090da7
Miles Kingston <miles.kingston@hotmail.com>
2015-08-18 15:45:53 +0100

    target w3schools customer json file

--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,17 @@
 var myApp = angular.module('myApp', []);
-
 myApp.controller('myAppCtrl', function($scope, $http) {
-	$http.get('phones.json').success(function(data) {
+	$http.get('phones.json')
+	.success(function(data, status, headers, config) {
 		$scope.phone = data;
 	});
-	$scope.orderProp = 'age';
-
-	// $scope.phones = [
-	//     {'name': 'Nexus S',
-	//      'snippet': 'Fast just got faster with Nexus S.',
-	//      'age': '7'},
-	//     {'name': 'Motorola XOOM with Wi-Fi',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//  	'age': '3'},
-	//     {'name': 'MOTOROLA XOOM',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//  	'age': '2'},
-	//     {'name': 'LG something',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//      'age': '5'},
-	//     {'name': 'Samsung Galaxy 6',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//      'age': '4'},
-	//     {'name': 'iPhone 6',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//      'age': '1'},
-	//     {'name': 'Nokia 3310',
-	//      'snippet': 'The Next, Next Generation tablet.',
-	//      'age': '8'},
- //  	];		
+	.error(function(data, status, headers, config) {
+    	console.log('error');
+  	});
+	// $scope.orderProp = 'age';		
 });
+
+myApp.config(['$httpProvider', function($httpProvider) { // For Chrome
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
