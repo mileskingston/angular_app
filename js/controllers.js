c541f816f4acfa0286de2c6ad045d02c33a826d2
Miles Kingston <miles.kingston@hotmail.com>
2015-08-18 15:49:16 +0100

    moved script to controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,11 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('myAppCtrl', function($scope, $http) {
-	$http.get('phones.json')
-	.success(function(data, status, headers, config) {
-		$scope.phone = data;
-	});
-	.error(function(data, status, headers, config) {
-    	console.log('error');
-  	});
-	// $scope.orderProp = 'age';		
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+	$http.get("http://www.w3schools.com/angular/customers.php")
+    	.success(function (data) { $scope.names = data.records; })
+   		.error(function (data) { console.log('error'); });
 });
 
-myApp.config(['$httpProvider', function($httpProvider) { // For Chrome
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
+app.config(['$httpProvider', function($httpProvider) { // For Chrome
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
